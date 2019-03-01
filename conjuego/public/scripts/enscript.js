65fa596120d9698f226364c933035342d350aadc
xander <jxandermorrison@gmail.com>
2019-03-01 16:41:35 -0600

    Requiring correct answer before allowing advancement

--- conjuego/public/scripts/enscript.js
+++ conjuego/public/scripts/enscript.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+	refreshPage();
+
 	function refreshPage() {
 		$("#continue").hide();
 		$("#respuesta").html("");
@@ -13,33 +15,44 @@ $(document).ready(function() {
 			$("#aspecto").html(result.mood);
 			$("#tenso").html(result.tense);
 
-			var answer = result.subject + " " + result.answer;
-			answer = answer.toUpperCase();
+			if (result.tense.toUpperCase() != "Afirmativo") {
+				var answer = result.subject + " " + result.answer;
+				var response = answer.charAt(0).toUpperCase();
+				response += answer.slice(1);
+			} else {
+				var answer = result.answer;
+				var response = answer.charAt(0).toUpperCase();
+				response += answer.slice(1);
+			}
+
+			$("#respuesta").data("data-answer", response);
 			
-			$("#guessForm").submit(function(e) {
-				e.preventDefault();
-
-				var values = $(this).serializeArray()[0];
-
-				var guess = values.value;
-				var check = guess.toUpperCase();
-
-				if (check == answer) {
-					$("#respuesta").html("CORRECTA<br>" + answer);
-				} else {
-					$("#respuesta").html("Lo sentimos<br>" + answer);
-				}
-				
-				$("#guess").blur();
-				$("#continue").fadeIn();
-
-				$("#entregar").prop("disabled", true);
-
-			});
 		});
 	}
 
-	refreshPage();
+	$("#guessForm").submit(function(e) {
+		e.preventDefault();
+
+		var values = $(this).serializeArray()[0];
+
+		var guess = values.value;
+		guess = guess.toUpperCase();
+
+		var answer = $("#respuesta").data("data-answer");
+		var check = answer.toUpperCase();
+
+		if (check == guess) {
+			$("#respuesta").html("CORRECTA<br>" + answer);
+			$("#guess").blur();
+			$("#continue").fadeIn();
+			$("#entregar").prop("disabled", true);
+
+		} else {
+			$("#respuesta").html("Lo sentimos<br>" + answer);
+			$("#guess").val("");
+		}
+	});
+
 
 	$(document).on("keypress", function(e) {
 		if (e.which === 13 && $("#continue").is(":visible")) {
@@ -50,5 +63,4 @@ $(document).ready(function() {
 	$("#continue").click(function() {
 		refreshPage();
 	});
-
 });
